953b420b15c3050c786344a769dec2f79ec6e9e5
Juho Choi <juho.c98@gmail.com>
2024-11-12 18:03:50 -0500

    .

--- actual/1_basics/10_array_methods.js
+++ actual/1_basics/10_array_methods.js
@@ -81,11 +81,102 @@ When comparing a and b, the compare function (a, b) => a - b works as follows:
 - If a - b == 0, a and b are equal, so their relative order
   doesn't change
 */
-let number = [1, 4, 3, 6, 7, 5, 9];
+let nums = [1, 4, 3, 6, 5, 9];
+nums.sort((a, b) => (a > b ? 1 : -1)); // ascending
+nums.sort((a, b) => (a > b ? -1 : 1)); // descending
 
 console.log("-----------------");
+
 // reverse(): reverses the order of the elements in an array in place, meaning
 // modifies the original array
 // It is often used after sort() to get a descending order of elements
 daysInWeek.reverse();
 console.log(daysInWeek); // ['Wed', 'Tue', 'Thu', 'Sun', 'Sat', 'Mon', 'Fri']
+
+console.log("-----------------");
+
+// map(): creates a new array populated with the results of
+// calling a provided function on every element in the calling array.
+// It does not modify the original array
+console.log(nums); // [ 9, 6, 5, 4, 3, 1 ]
+console.log(nums.map((num) => num * 2)); // [ 18, 12, 10, 8, 6, 2 ]
+
+console.log("-----------------");
+
+// filter(): creates a new array with all elements that pass a test
+// without modifying the orginal array
+console.log(nums.filter((x) => x % 2 === 0)); // [ 6, 4 ]
+
+// find(): searches for a specific element in an array that meets
+// a certain condition. It returns the first element that satisfies
+// the condition; returns undefined otherwise
+console.log(nums.find((x) => x % 2 === 0)); // 6
+
+// findIndex: searches for a specific element in an array that meets
+// a certain condition, and returns its index; -1 otherwise.
+console.log(nums.findIndex((x) => x % 2 === 0)); // 1
+
+console.log("-----------------");
+
+/*
+reduce(): processes elements in an array and reduce them to
+a single accumulated value.
+
+array.reduce(callback(acc, curValue, index: opt, array: opt), initValue: opt) => acc
+
+- When initialValue is provided:
+  reduce uses initialValue as the starting accumulator value.
+  The index for currentValue starts from 0 because the reduction process 
+  begins with the first element of the array.
+
+- When initialValue is not provided:
+  reduce takes the first element of the array as the initial accumulator value.
+  The index for currentValue starts from 1 because the reduction skips 
+  the first element, treating it as the accumulator directly.
+*/
+
+// Summing Numbers
+let sum = nums.reduce((acc, curVal) => acc + curVal, 0);
+console.log(sum); // 28
+
+sum = nums.reduce((acc, curVal) => acc + curVal);
+console.log(sum); // 28
+
+console.log("-----------------");
+
+nums = [5, 10, 15, 20];
+
+const sumWithInitVal = nums.reduce((acc, curVal, index) => {
+  console.log(`Index: ${index}, Accumulator: ${acc}, Current Value: ${curVal}`);
+  return acc + curVal;
+}, 0);
+
+console.log(`Total Sum: ${sumWithInitVal}`);
+
+/*
+Index: 0, Accumulator: 0, Current Value: 5
+Index: 1, Accumulator: 5, Current Value: 10
+Index: 2, Accumulator: 15, Current Value: 15
+Index: 3, Accumulator: 30, Current Value: 20
+Total Sum: 50
+*/
+
+const sumWithoutInitVal = nums.reduce((acc, curVal, index) => {
+  console.log(`Index: ${index}, Accumulator: ${acc}, Current Value: ${curVal}`);
+  return acc + curVal;
+});
+
+console.log(`Total Sum: ${sumWithoutInitVal}`);
+
+/*
+Index: 1, Accumulator: 5, Current Value: 10
+Index: 2, Accumulator: 15, Current Value: 15
+Index: 3, Accumulator: 30, Current Value: 20
+Total Sum: 50
+*/
+
+console.log("-----------------");
+
+nums = [1, 8, 3, 5, 9];
+const max = nums.reduce((acc, curVal) => Math.max(acc, curVal));
+console.log(max);
