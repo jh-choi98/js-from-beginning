3032fb9578dc700c30f14636b848da24fcb6770e
Juho Choi <juho.c98@gmail.com>
2024-11-18 00:54:50 -0500

    .

---
+++ actual/all_about_object/1_intro.js
@@ -0,0 +1,38 @@
+/*
+객체를 선언할 때 사용할 수 있는 방법
+1. object를 생성해서 객체 생성
+2. class를 인스턴스화해서 생성
+3. function을 사용해서 객체 생성 - 생성자 함수
+*/
+
+// 1. object를 생성해서 객체 생성
+const juho = {
+  name: "Juho",
+  year: 1998,
+};
+
+// 2. class를 인스턴스화해서 생성
+class Person {
+  name;
+  year;
+
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+const juho2 = new Person("Juho", 1998);
+
+// 3. function을 사용해서 객체 생성 - 생성자 함수
+function PersonFunction(name, year) {
+  this.name = name;
+  this.year = year;
+
+  this.greet = function () {
+    console.log(
+      `Hello, my name is ${this.name} and I was born in ${this.year}`
+    );
+  };
+}
+const juho3 = new PersonFunction("juho", 1998);
+juho3.greet();
